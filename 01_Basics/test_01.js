ff6393842810e11615bb4a1f8854b7e6f3d80ac0
Sai Prasad Prayaga <123971138+PrayagaSai@users.noreply.github.com>
2024-11-07 07:21:45 +0000

    Commited

--- 01_Basics/test_01.js
+++ 01_Basics/test_01.js
@@ -1,1 +1,20 @@
-console.log("Sai Prasad");
+let score = 200;
+const name = "Sai Prasad";
+
+// console.log(score);
+// console.log(name);
+
+let num = new Number(5000.64);
+// console.log(num.toString());
+// console.log(num.toString().length);
+console.log(num.toFixed());
+console.log(num.toPrecision(4));
+
+
+let date = new Date;
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toTimeString());
+// console.log(date.toISOString());
+
